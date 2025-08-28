857a8624071f3fe2af7b7e7ca3193da4b6d288f7
VladiSH31 <vladislav.sheremet@ukr.net>
2025-08-28 13:52:08 +0300

    FE. NextJS. Class Work. Lesson 3 ISR 1. {next: {revalidate: 5 }} -static and revalidate 5s 2. {revalidate: 0 }} - dynamic 3. {cache: 'no-cache'} - dynamic

--- src/app/(public)/users/page.tsx
+++ src/app/(public)/users/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const UsersPage = async () => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-cache'})
         .then((response) => response.json())
 
     return (
         <div>
+            <h2>{Date.now()}</h2>
             {
                 users.map((user: {id: number, name: string}) => <div key={user.id}>
                     {user.name}
